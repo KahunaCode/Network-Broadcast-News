54a9d008015fc58f0649524737d165e67ec1895b
James Moore <jameslmoore@gmail.com>
2017-07-01 20:00:32 -1000

    added client object, need to get nickname

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-var name = Math.floor(Math.random() * 1000) + 1
+//var name = Math.floor(Math.random() * 1000) + 1
 
 const socket = net.connect(6969, '0.0.0.0', () => {
   console.log("client connected");
@@ -17,7 +17,8 @@ process.stdin.on('readable', () => {
   var chunk = process.stdin.read();
   if (chunk !== null) {
     //process.stdout.write("stdout: "+chunk);
-    socket.write("RandoMan"+name+' '+chunk);
+    //socket.write("RandoMan"+name+' '+chunk);
+    socket.write(chunk);
   }
 });
 
--- js/server.js
+++ js/server.js
@@ -5,16 +5,61 @@ var client_list = [];
 const server = net.createServer((c) => {
   console.log(c.remoteAddress+' '+c.remotePort+' now connected');
 
-  client_list.push(c);
+  var nameChosen = false;
 
+  //client_list.push(c);
+
+  //get data from client socket, then pass to other clients
   c.on('data', function(data) {
       process.stdout.write(data);
+      if (nameChosen === false){
+        var nick = data.toString();
+
+        client_list.push({
+          nickname:nick,
+          socket:c
+        });
+
+        console.log(client_list);
+        nameChosen = true;
+      }
+      //look for server commands that start with '/'
+      // var slash = data.toString().split(' ')[1];
+      // if (slash === '/nick') {
+      //   var nick = data.toString().split(' ')[2];
+      //   console.log("changing nickname to: "+nick);
+      //   client_list.push({nick:c});
+      //   console.log(client_list);
+      // }
+
+      //console.log(data);
       client_list.forEach(function(client){
-        if (client === c) return;
-        client.write(data);
+        //console.log(client.socket);
+        if (client.socket === c) return;
+        if (client.nickname){
+          var nn = client.nickname;
+          client.socket.write(nn+': '+data);
+        }else{
+          client.socket.write(data);
+        }
+
+        //client.socket.write(nn+': '+data);
       });
+
+
+
+      // client_list.forEach(function(client){
+      //   for (var p in client.socket){
+      //     if (client.socket.hasOwnProperty(p)){
+      //       console.log("p is", p);
+      //       if (p === c) return;
+      //       p.write(data);
+      //     }
+      //   }
+      // });
     });
 
+  //broadcast Admin messages
   process.stdin.setEncoding('utf8');
   process.stdin.on('readable', () => {
     var chunk = process.stdin.read();
@@ -32,7 +77,7 @@ const server = net.createServer((c) => {
     console.log("removed connection");
   });
 
-  c.write('Welcome to JChat\r\n');
+  c.write('Welcome to JChat. Enter nickname:\r\n');
   });
 
 server.on('error', (err) => {
